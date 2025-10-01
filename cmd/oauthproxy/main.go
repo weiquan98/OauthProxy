f5bc4caa0d5572378c603b5ba7efa1cfc728aab1
Quan <30767205+weiquan98@users.noreply.github.com>
2025-10-01 18:55:37 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/oauthproxy/main.go
@@ -0,0 +1,23 @@
+// cmd/oauthproxy/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "oauthproxy/internal/oauthproxy"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := oauthproxy.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
